059cae295549b200eef7fef1d299d9add46e236f
João Pedro Sardou de Oliveira <joaopso2@al.insper.edu.br>
2025-10-20 16:41:54 -0300

    Terceira tentativa

--- main/pins.h
+++ main/pins.h
@@ -1,12 +1,12 @@
 #ifndef PINS_H
 #define PINS_H
 
-#define int BTN_PIN_R  4
-#define int BTN_PIN_G  5
-#define int BTN_PIN_B  6
-#define int LED_PIN_R  7
-#define int LED_PIN_G  8
-#define int LED_PIN_B  9
+#define  BTN_PIN_R  4
+#define  BTN_PIN_G  5
+#define  BTN_PIN_B  6
+#define  LED_PIN_R  7
+#define  LED_PIN_G  8
+#define  LED_PIN_B  9
 
 
 #endif // PINS_H
